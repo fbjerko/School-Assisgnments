15cf3bb897d9e993171041f1debe2cd47674459a
fbjerko <35508163+fbjerko@users.noreply.github.com>
2018-08-14 15:45:36 +0200

    Add files via upload

---
+++ Algorithms and Data Structures - Java/Linked-List Implementation/Node.java
@@ -0,0 +1,45 @@
+
+public class Node{
+		
+	int data;
+	public Node next;
+	
+	public Node(int data){
+		this.data = data;	
+	}
+	
+	public Node(Node next){
+		this.next = next;
+	}
+	
+	public Node(){
+		
+	}
+
+	public String toString(){
+		return data + "";
+	}	
+}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
